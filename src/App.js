5cb112bc6a54e58949ee62f03f270a666575bddd
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2022-04-10 12:44:13 +0200

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Login from "./Login";
 import Navigation from "./Navigation/Navigation";
 
 function App() {
   return (
-    <Navigation />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigation />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
