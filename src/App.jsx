f450808ebeddce74ad8ea9ee08c048ad9bc2554d
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2022-04-22 17:31:01 +0200

    add routing for logged in user

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import {
   Routes,
   Route,
+  Navigate,
 } from "react-router-dom";
-import { ThemeProvider } from "@mui/material";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 
 import theme from "./theme";
-import Login from "./Login";
-import Navigation from "./Navigation/Navigation";
+import Login from "./components/Login";
+// import Navigation from "./components/Navigation/Navigation";
+import { PrivateOutlet, PublicOutlet } from "./components/Outlets";
+import { ROUTES } from "./config";
 
-const Success = () => <div>Logged in</div>;
+// const Success = () => <div>Logged in</div>;
 
 const App = () => (
   <ThemeProvider theme={theme}>
+    <CssBaseline />
     <Routes>
-      <Route path="/" element={<Navigation />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/login/success" element={<Success />} />
+      <Route element={<PublicOutlet />}>
+        <Route path={ROUTES.SIGN_IN} element={<Login />} />
+      </Route>
+      <Route path="/" element={<PrivateOutlet />}>
+        <Route index element={<p>Not logged in</p>} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   </ThemeProvider>
 );
--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -1,28 +1,14 @@
-// import { Outlet, useMatch } from "react-router-dom";
-// import CssBaseline from "@mui/material/CssBaseline";
-// import Box from "@mui/material/Box";
+import Box from "@mui/material/Box";
 
-// import Sidebar from "../components/Sidebar";
-// import { ROUTES } from "../config";
-// import Navbar from "../components/Navbar";
+import Navigation from "../components/Navigation/Navigation";
 
-// function PrivateLayout() {
-//   return (
-//     <Box sx={{ display: "flex", backgroundColor: "gray.light", height: "100vh" }}>
-//       <CssBaseline />
-//       {!useMatch({ path: ROUTES.CUSTOMERS, end: true }) && <Navbar />}
-//       {!useMatch({ path: ROUTES.CUSTOMERS, end: true }) && <Sidebar />}
-//       <Box
-//         component="main"
-//         sx={{
-//           flexGrow: 1, pl: "45px", pt: 12, backgroundColor: "gray.light", display: "flex",
-// flexDirection: "column",
-//         }}
-//       >
-//         <Outlet />
-//       </Box>
-//     </Box>
-//   );
-// }
+const PrivateLayout = () => (
+  <>
+    <Navigation />
+    <Box sx={{ display: "flex", backgroundColor: "gray.light", height: "100vh" }}>
+      Private layout
+    </Box>
+  </>
+);
 
-// export default PrivateLayout;
+export default PrivateLayout;
